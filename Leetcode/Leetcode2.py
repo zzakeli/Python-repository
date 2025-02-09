b20678d1dc4917b5133f2b765d128210822150ac
Jzy <jestalycastillo15@gmail.com>
2025-02-09 13:38:12 +0800

    solved leetcode #1678

---
+++ Leetcode/Leetcode2.py
@@ -0,0 +1,22 @@
+def interpret(self, command):
+    result = ""
+
+    for i in range(len(command)):
+        if(command[i] == "G"):
+            result += "G"
+            continue
+        
+        if(command[i : i + 2] == "()"):
+            result += "o"
+            i += 1
+            continue
+        
+        if(command[i : i + 4] == "(al)"):
+            result += "al"
+            i += 3
+    
+    return result
+
+command  = "G()()()()(al)"
+self = 0
+print(interpret(self, command))
